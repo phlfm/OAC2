d0f9850a30a2348e2e68c5a3e2f7732ba21c3618
PHLFM_Darwin <pedrinho.mendonca@gmail.com>
2019-11-02 20:52:42 -0300

    Correção de typos nas variavies de control.vhd e tirando caracteres nao ascii de branch_table

--- branch_table.vhd
+++ branch_table.vhd
@@ -14,7 +14,7 @@
 --
 -------------------------------------------------------------------------------
 --
--- Description : 
+-- Description :
 --
 -------------------------------------------------------------------------------
 
@@ -26,10 +26,10 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity branch_table is
-	 port(	 
-         clock:    in 	bit; 
-         reset:	  in 	bit; 
-		 
+	 port(
+         clock:    in 	bit;
+         reset:	  in 	bit;
+
 		 branch_result : in bit;   -- Zero???
 		 branch_instruction: in bit; --Controle
 		 prediction : out bit
@@ -38,7 +38,7 @@ end branch_table;
 
 --}} End of automatically maintained section
 
-architecture branch_table of branch_table is 
+architecture branch_table of branch_table is
 
 	component reg is
         generic(wordSize: natural :=4);
@@ -47,62 +47,62 @@ architecture branch_table of branch_table is
             reset:	  in 	bit; --! clear assÃ­ncrono
             load:     in 	bit; --! write enable (carga paralela)
             d:   			in	bit_vector(wordSize-1 downto 0); --! entrada
-            q:  			out	bit_vector(wordSize-1 downto 0) --! saÃ­da
+            q:  			out	bit_vector(wordSize-1 downto 0) --! saida
         );
-    end component;	
-	
-	
-	-- Estados: 00 => Fortemente não tomar
-	--          01 => Fracamente não tomar	
+    end component;
+
+
+	-- Estados: 00 => Fortemente nao tomar
+	--          01 => Fracamente nao tomar
 	--          10 => Fracamente tomar
 	--          11 => Fortemente tomar
 	signal state : bit_vector(1 downto 0) := "01";
-	signal next_state : bit_vector(1 downto 0);		 
-	
-	signal test_result : bit_vector(1 downto 0);	
+	signal next_state : bit_vector(1 downto 0);
+
+	signal test_result : bit_vector(1 downto 0);
 	signal Lff, Lfv, Lvf, Lvv : bit_vector(1 downto 0);
-	
-begin	
-	
-	-- Teste 0: Se instrução é de desvio  
+
+begin
+
+	-- Teste 0: Se instrucao eh de desvio
 	test_result(0) <= branch_instruction;
-	
-	-- Teste 1: Se desvio foi tomado	  
+
+	-- Teste 1: Se desvio foi tomado
 	test_result(1) <= branch_result;
-	
-	
-	
-	-- Lff -> Instrução não é de desvio. Estado se mantém
+
+
+
+	-- Lff -> Instruï¿½ï¿½o nï¿½o ï¿½ de desvio. Estado se mantï¿½m
 	Lff <= state;
-	
-	-- Lvf -> Instrução não é de desvio. Estado se mantém. 
-	-- A flag de desvio foi para 1, mas em função de outra instrução, que não tem nada a ver com desvio
-	Lvf <= state;	   
-	
-	-- Lfv -> Instrução é de desvio e o desvio NÃO foi tomado
+
+	-- Lvf -> Instruï¿½ï¿½o nï¿½o ï¿½ de desvio. Estado se mantï¿½m.
+	-- A flag de desvio foi para 1, mas em funï¿½ï¿½o de outra instruï¿½ï¿½o, que nï¿½o tem nada a ver com desvio
+	Lvf <= state;
+
+	-- Lfv -> Instruï¿½ï¿½o ï¿½ de desvio e o desvio Nï¿½O foi tomado
 	with state
-	select Lfv <= 	"00" when "00",	
-				  	"00" when "01",	 
-	 				"01" when "10",	
+	select Lfv <= 	"00" when "00",
+				  	"00" when "01",
+	 				"01" when "10",
 				  	"10" when "11",
 					"00" when others;
-	
-	-- Lfv -> Instrução é de desvio e o desvio FOI tomado
+
+	-- Lfv -> Instruï¿½ï¿½o ï¿½ de desvio e o desvio FOI tomado
 	with state
-	select Lvv <=  	"01" when "00",	
-				  	"10" when "01",	 
-	 				"11" when "10",	
+	select Lvv <=  	"01" when "00",
+				  	"10" when "01",
+	 				"11" when "10",
 				  	"11" when "11",
 					"00" when others;
-	  
+
 	-- MUX com os Links
 	with test_result
-	select next_state <=	Lff when "00",	
-						  	Lfv when "01",	 
-			 				Lvf when "10",	
+	select next_state <=	Lff when "00",
+						  	Lfv when "01",
+			 				Lvf when "10",
 						  	Lvv when "11",
 							"01" when others;
-					
+
 	state_reg : reg generic map (2) port map (
 		clock => clock,
 		reset => reset,
@@ -110,9 +110,8 @@ begin
 		d => next_state,
 		q => state
 	);
-	
-	
-	prediction <= state(0);
-	
-end branch_table;						   
 
+
+	prediction <= state(0);
+
+end branch_table;
--- control.vhd
+++ control.vhd
@@ -4,19 +4,19 @@ library ieee;
 use ieee.numeric_bit.all;
 
 entity control is
-	port (	
+	port (
 		clock : in bit;
 		reset: in bit;
-	
+
 	    instruction31to21: in bit_vector(10 downto 0);
-		 
+
 		AluSrc: out bit;		      		--EX
 	    AluOP: out bit_vector(1 downto 0);	--EX
-	    
+
 		branch: out bit;					--MEM
 		MemRead: out bit;					--MEM
 	    MemWrite: out bit; 					--MEM
-		
+
 	    RegWrite: out bit;					--WB
 	    MemToReg: out bit					--WB
 	);
@@ -26,21 +26,21 @@ architecture control of control is
 	signal R : bit;
 	signal LDUR : bit;
 	signal STUR : bit;
-	signal CBZ : bit;	  
-	
+	signal CBZ : bit;
+
 	signal temp_AluSrc: 	bit;		      		--EX
 	signal temp_AluOP: 		bit_vector(1 downto 0);	--EX
 	signal temp_branch: 	bit;					--MEM
 	signal temp_MemRead: 	bit;					--MEM
 	signal temp_MemWrite: 	bit; 					--MEM
 	signal temp_RegWrite: 	bit;					--WB
-	signal temp_MemToReg: 	bit;					--WB   
-	
+	signal temp_MemToReg: 	bit;					--WB
+
 	signal DECODE_data:		bit_vector(7 downto 0);
 	signal EX_Reg:			bit_vector(7 downto 0);
-	signal MEM_Reg:			bit_vector(4 downto 0);	
+	signal MEM_Reg:			bit_vector(4 downto 0);
 	signal WB_Reg:			bit_vector(1 downto 0);
-	
+
 	component reg is
         generic(wordSize: natural :=4);
         port(
@@ -51,43 +51,43 @@ architecture control of control is
             q:              out bit_vector(wordSize-1 downto 0) --! saída
         );
     end component;
-	
+
 begin
 	-- CBZ  = "10110100XXX" => 10X0001001
 	-- STUR = "11111000000" => 11X0010000
 	-- LDUR = "11111000010" => X111100000
 	-- R    = "1XX0101X000" => 0001000010
-	
-	
+
+
 	R		<= '1' when instruction31to21(10) = '1' and instruction31to21(7 downto 4) = "0101" and instruction31to21(2 downto 0) = "000" else '0';
 	LDUR	<= '1' when instruction31to21 = "11111000010" else '0';
 	STUR	<= '1' when instruction31to21 = "11111000000" else '0';
 	CBZ	 	<= '1' when instruction31to21(10 downto 3) = "10110100" else '0';
-	
-	
+
+
 	--reg2loc	<= STUR or CBZ; -- STUR | CBZ
 
-	temp_aluSrc		<= LDUR or STUR; 	-- LDUR | STUR
-	
-	temp_memToReg	<= LDUR; 			-- LDUR
-	temp_regWrite	<= R or LDUR; 		-- R | LDUR
-	
-	temp_memRead	<= LDUR; 			-- LDUR
-	temp_memWrite 	<= STUR; 			-- STUR
+	temp_AluSrc		<= LDUR or STUR; 	-- LDUR | STUR
+
+	temp_MemToReg	<= LDUR; 			-- LDUR
+	temp_RegWrite	<= R or LDUR; 		-- R | LDUR
+
+	temp_MemRead	<= LDUR; 			-- LDUR
+	temp_MemWrite 	<= STUR; 			-- STUR
 
 	temp_branch		<= CBZ; 			-- CBZ
 	--uncondBranch <= '0'; -- ?
-	
+
 	temp_AluOP(1)	<= R; 				-- R
 	temp_AluOP(0)	<= CBZ;				-- CBZ
-	
-	
-	
-	
+
+
+
+
 	--REGISTRADORES DO PIPELINE
-	
-	DECODE_data <= temp_AluSrc & temp_AluOP & temp_branch & temp_MemRead & temp_MemWrite & temp_RegWrite & temp_MemToReg;
-	
+
+	DECODE_data <= temp_AluSrc & temp_AluOP & temp_branch & temp_MemRead & temp_MemWrite & temp_RegWrite & temp_MemToReg; -- concat de bits: https://stackoverflow.com/questions/209458/concatenating-bits-in-vhdl
+
 	regEX : reg generic map (8)
 	port map(
 		clock => clock,
@@ -96,10 +96,10 @@ begin
 		d => DECODE_data,
 		q => EX_Reg
 	);
-	
-	AluSrc <= EX_Reg(7);			
-	AluOP <= EX_Reg(6 downto 5);  
-	
+
+	AluSrc <= EX_Reg(7);
+	AluOP <= EX_Reg(6 downto 5);
+
 	regMEM : reg generic map (5)
 	port map(
 		clock => clock,
@@ -107,12 +107,12 @@ begin
 		load => '1',
 		d => EX_Reg(4 downto 0),
 		q => MEM_Reg
-	);				
-	
+	);
+
 	branch <= MEM_Reg(4);
 	MemRead <= MEM_Reg(3);
-	MemWrite <= MEM_Reg(2);	
-	
+	MemWrite <= MEM_Reg(2);
+
 	regWB : reg generic map (2)
 	port map(
 		clock => clock,
@@ -120,9 +120,9 @@ begin
 		load => '1',
 		d => MEM_Reg(1 downto 0),
 		q => WB_Reg
-	);			  
-	
+	);
+
 	RegWrite <= WB_Reg(1);
 	MemToReg <= WB_Reg(0);
-	
+
 end architecture control;
