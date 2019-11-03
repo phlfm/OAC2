bda5f8c2a64d5dd3b0e0d5c46af8a990bb03b135
PHLFM_Darwin <pedrinho.mendonca@gmail.com>
2019-11-03 20:40:22 -0300

    implementei quase tudo do branch_table, falta apenas inserir novas entradas a partir do execute e, caso nao haja espaco, substituir a entrada menos utilizada pela entrada nova

--- branch_table.vhd
+++ branch_table.vhd
@@ -61,6 +61,11 @@ architecture branch_table of branch_table is
 begin
 
 tableProc: process(clock, reset) is
+variable current_stateW : bit_vector(1 downto 0) := ( others => '0');
+variable next_stateW : bit_vector(1 downto 0) := ( others => '0');
+variable read_prediction: bit := '0';
+variable read_branchAddr : bit_vector(addrSize-1 downto 0) := ( others => '0');
+
 begin
 if reset = '1' then
 	state <= ( others => '0');
@@ -69,10 +74,77 @@ if reset = '1' then
 	selector <= ( others => '0');
 else -- else do reset
 	if rising_edge(clock) then
-
+---------------------------------------------------
+------    WRITE    ---------------------------------
+---------------------------------------------------
+-- TODOS OS INDICES DAQUI P BAIXO DEVEM SER iW (ate a secao de read)
+		-- faz write e depois read
+		if branch_instruction = '1' then
+			-- faz um for procurando o addrW (input) na tabela
+			search_instruction_addr_on_write: for iW in (tableSize-1) to 0 loop
+			-- se addr(tabela) = addrW(input), entao atualiza o estado
+				if instruction_addr(addrSize*(iW+1)-1 downto addrSize*iW) = instruction_addrW then
+					current_stateW := state((iW+1)*2-1 downto (iW+1)*2-2);
+				-- atualiza o estado
+					if branch_result = '1' then
+					-- incrementa o estado
+						case current_stateW is
+							when "00" => next_stateW := "01";
+							when "01" => next_stateW := "10";
+							when others => next_stateW := "11";
+						end case;
+					else
+					-- decrementa o estado
+						case current_stateW is
+							when "11" => next_stateW := "10";
+							when "10" => next_stateW := "01";
+							when others => next_stateW := "00";
+						end case;
+				 	end if; -- if da atualizacao de estado (branch_result)
+					-- atualiza o estado de fato:
+					state((iW+1)*2-1 downto (iW+1)*2-2) <= next_stateW;
+				else -- NAO encontrou a entrada no buffer, criar uma nova:
+					-- TODO: criar nova entrada
+				end if; -- if addr(tabela) = addrW(input)
+			end loop search_instruction_addr_on_write;
+		end if; -- if branch_instruction (eh o write)
+---------------------------------------------------
+------    READ    ---------------------------------
+---------------------------------------------------
+-- TODOS OS INDICES DAQUI P BAIXO DEVEM SER iR
+		-- ja fez write, agora faz read
+		-- faz um for procurando o addrW (input) na tabela
+		search_instruction_addr_on_read: for iR in (tableSize-1) to 0 loop
+		-- se addr(tabela) = addrW(input), entao atualiza o estado
+			if instruction_addr(addrSize*(iR+1)-1 downto addrSize*iR) = instruction_addrR then
+				current_stateW := state((iR+1)*2-1 downto (iR+1)*2-2);
+				read_prediction := current_stateW(1);
+				read_branchAddr := branch_addr(addrSize*(iR+1)-1 downto addrSize*iR);
+				EXIT;
+			else -- NAO encontrou a entrada no buffer, fazer output da predicao = 0
+				read_prediction := '0';
+				read_branchAddr := (others => '0');
+			end if;  -- if addr(tabela) = addrR(input)
+		end loop search_instruction_addr_on_read;
+			branch_addrR <= read_branchAddr;
+			prediction <= read_prediction;
 	end if; -- rising_edge(clock)
 end if;	-- else do reset
 end process tableProc;
 
 
 end branch_table;
+
+-- ULTIMA COMPILACAO: MODELSIM 2019-11-03 20:39
+--vcom -reportprogress 300 -work work C:/temp/gitLEGv8/branch_table.vhd
+--# Model Technology ModelSim - Intel FPGA Edition vcom 10.5b Compiler 2016.10 Oct  5 2016
+--# Start time: 20:38:40 on Nov 03,2019
+--# vcom -reportprogress 300 -work work C:/temp/gitLEGv8/branch_table.vhd
+--# -- Loading package STANDARD
+--# -- Loading package TEXTIO
+--# -- Loading package std_logic_1164
+--# -- Loading package NUMERIC_BIT
+--# -- Compiling entity branch_table
+--# -- Compiling architecture branch_table of branch_table
+--# End time: 20:38:40 on Nov 03,2019, Elapsed time: 0:00:00
+--# Errors: 0, Warnings: 0
