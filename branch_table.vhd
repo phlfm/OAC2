1c85927279d15c86537349d8d15956a7087d1ccf
PHLFM_Darwin <pedrinho.mendonca@gmail.com>
2019-11-02 23:17:16 -0300

    Alterações propostas para o funcionamento do branch_table

--- branch_table.vhd
+++ branch_table.vhd
@@ -1,26 +1,13 @@
--------------------------------------------------------------------------------
---
--- Title       : branch_table
--- Design      : LEGv8
--- Author      : pliga@globo.com
--- Company     : USP
---
--------------------------------------------------------------------------------
---
--- File        : d:\Active_HDL\LEGv8\src\branch_table.vhd
--- Generated   : Sat Oct 19 18:51:17 2019
--- From        : interface description file
--- By          : Itf2Vhdl ver. 1.22
---
--------------------------------------------------------------------------------
---
--- Description :
---
--------------------------------------------------------------------------------
+-- Funcionamento:
 
---{{ Section below this comment is automatically maintained
---   and may be overwritten
---{entity {branch_table} architecture {branch_table}}
+-- Estagio de FETCH:
+-- writeEnable = 0, retorna prediction para addrR.
+-- (Se a instrucao nao ta na branch_table, retorna 0, tudo continua normal)
+-- (prediction eh consultado da tabela que eh escrita/populada no estagio execute)
+
+-- Estagio EXECUTE:
+-- Se for instrucao de branch, coloca o end em addrW, writeEnable = 1 e escreve branch_result
+-- O modulo vai guardar o addrW numa tabela junto com a maquina de estados de previsao associado a esse addr.
 
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -30,27 +17,23 @@ entity branch_table is
          clock:    in 	bit;
          reset:	  in 	bit;
 
-		 branch_result : in bit;   -- Zero???
-		 branch_instruction: in bit; --Controle
-		 prediction : out bit
+-- Esses sao usados no FETCH:
+		 instruction_addrR: in bit_vector(11 downto 0); -- end da inst atual
+		 branch_addrR : in bit_vector(11 downto 0); -- end para o qual desviar
+		 prediction : out bit; -- se deve desviar ou nao
+
+-- Esses sao usados no EXECUTE:
+		 branch_instruction: in bit; -- age como um enable de escrita
+		 branch_result : in bit; -- resultado do branch para atualizar
+		 instruction_addrW: in bit_vector(11 downto 0); -- end da inst atual
+		 branch_addrW : in bit_vector(11 downto 0) -- end para o qual desviar
+-- se instruction_addrW nao existir na tabela, ele cria uma nova entrada, se nao, so atualiza.
+
 	     );
 end branch_table;
 
---}} End of automatically maintained section
 
 architecture branch_table of branch_table is
-
-	component reg is
-        generic(wordSize: natural :=4);
-        port(
-            clock:    in 	bit; --! entrada de clock
-            reset:	  in 	bit; --! clear assíncrono
-            load:     in 	bit; --! write enable (carga paralela)
-            d:   			in	bit_vector(wordSize-1 downto 0); --! entrada
-            q:  			out	bit_vector(wordSize-1 downto 0) --! saida
-        );
-    end component;
-
 
 	-- Estados: 00 => Fortemente nao tomar
 	--          01 => Fracamente nao tomar
@@ -64,54 +47,5 @@ architecture branch_table of branch_table is
 
 begin
 
-	-- Teste 0: Se instrucao eh de desvio
-	test_result(0) <= branch_instruction;
-
-	-- Teste 1: Se desvio foi tomado
-	test_result(1) <= branch_result;
-
-
-
-	-- Lff -> Instru��o n�o � de desvio. Estado se mant�m
-	Lff <= state;
-
-	-- Lvf -> Instru��o n�o � de desvio. Estado se mant�m.
-	-- A flag de desvio foi para 1, mas em fun��o de outra instru��o, que n�o tem nada a ver com desvio
-	Lvf <= state;
-
-	-- Lfv -> Instru��o � de desvio e o desvio N�O foi tomado
-	with state
-	select Lfv <= 	"00" when "00",
-				  	"00" when "01",
-	 				"01" when "10",
-				  	"10" when "11",
-					"00" when others;
-
-	-- Lfv -> Instru��o � de desvio e o desvio FOI tomado
-	with state
-	select Lvv <=  	"01" when "00",
-				  	"10" when "01",
-	 				"11" when "10",
-				  	"11" when "11",
-					"00" when others;
-
-	-- MUX com os Links
-	with test_result
-	select next_state <=	Lff when "00",
-						  	Lfv when "01",
-			 				Lvf when "10",
-						  	Lvv when "11",
-							"01" when others;
-
-	state_reg : reg generic map (2) port map (
-		clock => clock,
-		reset => reset,
-		load => '1',
-		d => next_state,
-		q => state
-	);
-
-
-	prediction <= state(0);
 
 end branch_table;
